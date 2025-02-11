8e78a63e80faaa517afaa400509f9aa0b863f55a
shravan kumar <shravan95741998@gmail.com>
2025-02-11 22:10:02 +0530

    fi

---
+++ MyJsProject/project.js
@@ -0,0 +1,12 @@
+var fruits = ["mango", "ornge", "banana", "kiwi"]
+
+document.write(fruits + "<br>")
+
+for (index = 0; index <= fruits.length; index++) {
+
+    document.write(index + ":" + fruits[index] + "<br>")
+
+}
+
+
+
