40de30d15e5b7fb9c721f02ef450537fa5e223ed
shravan kumar <shravan95741998@gmail.com>
2025-02-11 22:41:46 +0530

    created video dir

---
+++ arrays_v12/project.js
@@ -0,0 +1,6 @@
+console.log("starting arrays 12 video");
+
+
+var movies = ["fidha", "f2", "f3"];
+console.log(movies);
+ 
---
+++ functions_v13/project.js
@@ -0,0 +1,3 @@
+console.log("starting functions 13 video");
+
+console.log("hello");
