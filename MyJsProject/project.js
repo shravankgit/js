e7fe6dbe32dce71292c35deb857e115f671fd2ef
shravan kumar <shravan95741998@gmail.com>
2025-02-11 22:17:27 +0530

    foroff

--- MyJsProject/project.js
+++ MyJsProject/project.js
@@ -3,10 +3,15 @@ var fruits = ["mango", "ornge", "banana", "kiwi"]
 document.write(fruits + "<br>")
 
 for (index = 0; index <= fruits.length; index++) {
-
-    document.write(index + ":" + fruits[index] + "<br>")
-
+    document.write(index + " ---- " + fruits[index] + "<br>");
 }
 
+
+document.write("mmm " + " nrspt ");
+
+
+for (const element of fruits) {
+    document.write(element + "<br>");
+}
 
 
