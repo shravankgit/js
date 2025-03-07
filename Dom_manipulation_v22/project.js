af6441fc6f7bf4cfb0b73bd25f4d26c3740cf67f
shravan kumar <shravan95741998@gmail.com>
2025-03-07 18:36:14 +0530

    model in js

---
+++ Dom_manipulation_v22/project.js
@@ -0,0 +1,8 @@
+
+var n = document.createElement("h1");
+n.innerText = "this is third paragraph in division2";
+console.log(n);
+var parant = document.getElementById("second");
+console.log(parent);
+
+
