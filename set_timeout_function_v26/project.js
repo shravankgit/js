ddc6bb916e62828aaa704af75c7d000250aa9fe7
shravan kumar <shravan95741998@gmail.com>
2025-03-11 21:05:17 +0530

    map

--- set_timeout_function_v26/project.js
+++ set_timeout_function_v26/project.js
@@ -5,11 +5,26 @@
 // console.log(x);
 // console.log("hello");
 
-var x = setTimeout(test, 3000, "xyz");
+// var x = setTimeout(test, 3000, "xyz");
+// console.log(x);
+
+// console.log("hello");
+
+// function test(name) {
+//     alert("Hello " + name);
+// }
+
+var counter = 1
+var x= setTimeout(test, 3000, "xyz");
 console.log(x);
 
-console.log("hello");
-
+console.log("hello")
 function test(name) {
-    alert("Hello " + name);
+
+alert("called:"+counter)
+counter++
+if(counter>5){
+    clearInterval(x)
 }
+}
+
